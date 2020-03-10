9e5c690c748cb7dc85ca9776a2c11fabb038a224
lucaspinheiro13 <58701015+lucaspinheiro13@users.noreply.github.com>
2020-03-10 16:58:00 -0300

    Atualização Regras de Mensagem

--- logic.js
+++ logic.js
@@ -12,30 +12,31 @@ function receiver (message,chatElem,title) {
       
       if (lcMessage.includes("ola bot")) {                  
                   var welcomeMessage = 'Digite seu nome.';
-                  Pessoa.atendimento = true;
+                  Pessoa.passo[0] = true;
                   //WhatsApee.send(chat Elem is like chat id , message to send goes here)               
                   WhatsApee.send(chatElem,welcomeMessage);                  
-      } 
-      if(Pessoa.atendimento&&!Pessoa.nome){
-            Pessoa.nome = lcMessage;
-            var message = lcMessage.toUpperCase()+" Em que posso ajudar?";
-            message += "1- Calculo Recisão";
-            message += "0- Voltar";
-            
-            WhatsApee.send(chatElem,message);
-      }
-      if(Pessoa.nome&&!Pessoa.acao){
-            Pessoa.acao = lcMessage;
-            
-            var message = "Insira seu salário";
-            WhatsApee.send(chatElem,message);
-      }
-      if(Pessoa.acao&&!Pessoa.salario){
-            Pessoa.salario = lcMessage;
+      } else {      
+            if (Pessoa.passo[0]) {
+                  Pessoa.nome = lcMessage;
+                  var message = lcMessage.toUpperCase()+" Em que posso ajudar?";
+                  message += "1- Calculo Recisão";
+                  message += "0- Voltar";
+                  
+                  WhatsApee.send(chatElem,message);
+            }
+            if (Pessoa.nome&&!Pessoa.passo[1]) {
+                  Pessoa.acao = lcMessage;
+                  Pessoa.passo[1]=true;
+                  var message = "Insira seu salário";
+                  WhatsApee.send(chatElem,message);
+            }
+            if (Pessoa.acao&&!Pessoa.passo[2]) {
+                  Pessoa.salario = lcMessage;
+                  Pessoa.passo[2];
+                  var message = Pessoa.salario*2/100;
+                  WhatsApee.send(chatElem,message);
 
-            var message = Pessoa.salario*2/100;
-            WhatsApee.send(chatElem,message);
-
+            }
       }
 
       if(lcMessage.includes("@time")){
