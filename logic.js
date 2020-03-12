00840b805450e7f5fbae0a90df25b7678384da99
lucaspinheiro13 <58701015+lucaspinheiro13@users.noreply.github.com>
2020-03-12 17:29:48 -0300

    Lógica dentro do IF do ola

--- logic.js
+++ logic.js
@@ -1,62 +1,64 @@
 const path = require('path');
 const WhatsApee = require(path.join(__dirname, 'WhatsApee.js'));
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-      host:'localhost',
-      user:'root',
-      password:'',
-      database:'rescisaowpp'
-});
-
-//WhatsAppe.start( receiver callback)
-WhatsApee.start(receiver);
 
 var cliente = function Cliente() {
-      this.nome
-      this.salario
-      this.ferias
-      this.decimo
+  this.nome
+  this.salario
+  this.ferias
+  this.decimo
 }
 
 const Pessoa = [];
+// const mysql = require('mysql');
 
-var valida = true;
+// const connection = mysql.createConnection({
+//       host:'localhost',
+//       user:'root',
+//       password:'',
+//       database:'rescisaowpp'
+// });
+
+//WhatsAppe.start( receiver callback)
+
+WhatsApee.start(receiver);
 // 
 function receiver (message,chatElem,title) {
-	 
+      
       var lcMessage = message.toLowerCase();
       
-      let nome = document.querySelector("#main > header > div._3V5x5 > div > div > span").innerHTML;
-
-      for (let i = 0; i <= Pessoa.length; i++) {
-            
-            if (Pessoa[i].nome == nome) {
-
-                var Cliente = Pessoa[i];
-
-                valida = false;
-
-                break;
-
-            } else {
-                valida = true;
-            }
-      }
-      if(valida = true){
-
-            Pessoa.push(new cliente());
-
-            var Cliente = Pessoa[Pessoa.length-1];
-
-            Cliente.nome = nome;
-
-      }
-
+     
       if (lcMessage.includes("ola")) {   
+            var valida = true ;
+            for ( i = 0; i < Pessoa.length; i++) {
+                  if(Pessoa[i]){
+                        if (Pessoa[i].nome == title) {
+            
+                        var Cliente = Pessoa[i];
+            
+                        valida = false;
+                        console.log('chegou auqi');
+                        break;
+            
+                        } else {
+                        
+                        valida = true;
+                        }
+                  }
 
-            var welcomeMessage = 'Olá,'+Cliente.nome;
-
+            }
+            console.log(valida);
+            if(valida = true){
+      
+                  Pessoa.push(new cliente());
+      
+                  var Cliente = Pessoa[Pessoa.length-1];
+      
+                  Cliente.nome = title;
+      
+            }
+            
+            var welcomeMessage = 'Olá, '+ Cliente.nome;
+            console.log(Pessoa);
             WhatsApee.send(chatElem,welcomeMessage );      
 
       } 
